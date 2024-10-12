c58d2dabb13d6b07b8b8cfcfaec60b71352ff221
Victor Lino <victor.lino@treeinova.com.br>
2024-10-12 16:24:09 -0300

    alteracao web project

--- src/BlogSharp.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogSharp.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
